3d3a9bc0a0806eeb3827d0e58e5f0e402161e08e
Yash <developer.yash.bhalani@gmail.com>
2023-05-29 11:50:04 -0300

    refactor: update function syntax

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,45 @@ import axios from "axios";
 
 function App() {
   const [shlokData, setShlokData] = useState({});
-  const baseUrl = "https://bhagavadgitaapi.in/slok/4/3/";
+  const [chapterNumber, setChapterNumber] = useState(4);
+  const [shlokNumber, setShlokNumber] = useState(12);
+
+  // Api Endpoint
+  const baseUrl = `https://bhagavadgitaapi.in/slok/${chapterNumber}/${shlokNumber}/`;
+
+  const generateRandom = (maxLimit = 18) => {
+    let rand = Math.random() * maxLimit;
+    rand = Math.floor(rand);
+    return rand;
+  };
+
   useEffect(() => {
     axios.get(baseUrl).then((response) => {
       setShlokData(response.data);
     });
-  }, []);
+  }, [baseUrl]);
+
+  const randomShlokGenereHandler = () => {
+    setChapterNumber(generateRandom());
+    setShlokNumber(generateRandom(20));
+  };
+
   return (
     <>
       <h1>Shlok Data</h1>
+      <button
+        onClick={() => {
+          randomShlokGenereHandler();
+        }}
+        style={{
+          padding: "10px 20px",
+          background: "#FFFFFF",
+          borderRadius: 8,
+          color: "#000000",
+        }}
+      >
+        Generate Random Shlok
+      </button>
       <h5>English</h5>
       <h3>{shlokData.adi?.et}</h3>
       <br />
